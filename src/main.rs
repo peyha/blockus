e2c9c279a185efe9357123d8c94b2072f76daf85
Peyha Nicole <peyha.nicole@outlook.fr>
2023-09-16 17:11:13 +0200

    feat(): add url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use reqwest;
+use std::time::{SystemTime};
+
+
 fn main() {
-    println!("Hello, world!");
+   
+    let request_url = format!("https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp={timestamp}&closest=before&apikey={apiKey}",
+        timestamp=SystemTime.now(),
+        apiKey='UBI18HPD3AFGC3QH6FZCHX423MKMYXHVCP');
+    
+    
 }
