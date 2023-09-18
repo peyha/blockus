3641d9fff87f508eb93b3cca2a9d3ee766288f84
Peyha Nicole <peyha.nicole@outlook.fr>
2023-09-18 21:45:44 +0200

    feat(*): print in a box

--- src/main.rs
+++ src/main.rs
@@ -58,13 +58,17 @@ async fn main() {
     let res = client.post(args.rpc_url.as_str()).json(&request).send().await.unwrap().text().await.unwrap();
     let data: Value = serde_json::from_str(&res).unwrap();
 
+
+
     loop{
         let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();       
         if block == 0{
             block = u64::from_str_radix(data["result"].as_str().unwrap().trim_start_matches("0x"), 16).unwrap();
         }
+        let mut texts = Vec::new();
+        texts.push(String::from("System info:"));
+        texts.push(String::from(format!("Timestamp: {}", now)));
 
-        println!("Currently at timestamp {}\nLast ethereum block was number {}", now, block);
 
         let request = GetBlockRequest{
             params: (format!("0x{:x}", block), true),
@@ -80,12 +84,19 @@ async fn main() {
         let hash = data["hash"].as_str().unwrap();
         let validator = data["miner"].as_str().unwrap();
         let txs = data["transactions"].as_array().unwrap();
-        println!("Block info:");
-        println!("Block hash: {}", hash);
-        println!("Block validator: {}", validator);
-        println!("Txs info:");
-        println!("Tx nb: {}", txs.len());
+        let block_size = u64::from_str_radix(data["size"].as_str().unwrap().trim_start_matches("0x"), 16).unwrap() / 1000;
+        let block_timestamp = u64::from_str_radix(data["timestamp"].as_str().unwrap().trim_start_matches("0x"), 16).unwrap();
         //println!("{:?}", txs[0]);
+        texts.push(String::from(""));
+        texts.push(String::from("Block info:"));
+        texts.push(String::from(format!("Block number: {}", block)));
+        texts.push(String::from(format!("Block hash: {}", hash)));
+        texts.push(String::from(format!("Block validator: {}", validator)));
+        texts.push(String::from(format!("Block size: {} kb", block_size)));
+        texts.push(String::from(format!("Block timestamp: {} ", block_timestamp)));
+        texts.push(String::from(""));
+        texts.push(String::from("Txs info:"));
+        texts.push(String::from(format!("Tx nb: {}", txs.len())));
 
         let mut min_gas = u64::MAX;
         let mut max_gas = u64::MIN;
@@ -122,16 +133,21 @@ async fn main() {
         
         avg_gas /= txs.len() as u64;
         avg_gas_price /= txs.len() as u64;
-        println!("transfer: {}, deployment: {}, execution: {}", type_count.0, type_count.1, type_count.2);
-        println!("Gas info:");
-        println!("Gas usage: min={}, max={}, avg={}", min_gas, max_gas, avg_gas);
+
+        texts.push(String::from(format!("transfer: {}, deployment: {}, execution: {}", type_count.0, type_count.1, type_count.2)));
+
+        texts.push(String::from(""));
+        texts.push(String::from("Gas info:"));
+        texts.push(String::from(format!("Gas usage: min={}, max={}, avg={}", min_gas, max_gas, avg_gas)));
+
         let (gas_target, gas_max) = (15000000, 30000000);
         let target_diff = (100 as f64)*((sum_gas as f64) - (gas_target as f64)) / (gas_target as f64);
         let max_diff = (100 as f64)*((sum_gas as f64) - (gas_max as f64)) / (max_gas as f64);
         //println!("Gas target: sum={}, {}% from target, {}% from max", sum_gas, target_diff, max_diff);
-        println!("Gas price: min={} Gwei, max={} Gwei, avg={} Gwei", (min_gas_price as f64) / 1e9, (max_gas_price as f64) / 1e9, (avg_gas_price as f64) / 1e9);
+        texts.push(String::from(format!("Gas price: min={} Gwei, max={} Gwei, avg={} Gwei", (min_gas_price as f64) / 1e9, (max_gas_price as f64) / 1e9, (avg_gas_price as f64) / 1e9)));
         
-        
+        print_in_box(texts);
+
         //sleeping until next block
         thread::sleep(Duration::from_secs(12));
         print!("{}[2J", 27 as char);
