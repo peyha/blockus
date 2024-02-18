b55fccd81c749824299816d753c88f2ff0dcded9
peyha <peyha.nicole@outlook.fr>
2024-02-18 19:45:21 +0100

    style: linter

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::time::SystemTime;
 use clap::Parser;
-use std::u64;
 use std::thread;
 use std::time::Duration;
+use std::time::SystemTime;
+use std::u64;
 
 mod utils;
 use utils::print_in_box;
@@ -14,7 +14,7 @@ use block_logic::{get_block_info, get_block_number};
 // CLI arguments
 #[derive(Parser)]
 #[command(author, version, about)]
-struct Cli{
+struct Cli {
     #[arg(short, long, default_value_t=String::from("https://eth.merkle.io"))]
     rpc_url: String,
 }
@@ -24,13 +24,15 @@ async fn main() {
     let args = Cli::parse();
     let mut block: u64 = 0;
 
-    loop{
-        let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();       
-        
+    loop {
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
         // Catching up to the last block every 20 blocks
         if block % 20 == 0 {
-
-            match get_block_number(args.rpc_url.as_str()).await{
+            match get_block_number(args.rpc_url.as_str()).await {
                 Ok(block_number) => block = block_number,
                 _ => {
                     println!("Error at time {}", now);
@@ -38,7 +40,7 @@ async fn main() {
                 }
             }
         }
-        match get_block_info(block, now, args.rpc_url.as_str()).await{
+        match get_block_info(block, now, args.rpc_url.as_str()).await {
             Ok(texts) => {
                 print_in_box(texts);
                 //sleeping until next block
@@ -47,8 +49,6 @@ async fn main() {
                 block += 1;
             }
             _ => println!("Error on block {}", block),
-        };  
-        
+        };
     }
-
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,8 @@
-use std::fmt::Display;
 use crate::block_logic::BlockRequestError;
 use serde_json::Value;
+use std::fmt::Display;
 
-// Convert an f64 value to a human readable format 
+// Convert an f64 value to a human readable format
 // e.g 523294.0223 = "523.3K"
 pub fn format_generic<T: Into<f64> + Display>(value: T) -> String {
     const K: f64 = 1_000.0;
@@ -23,26 +23,27 @@ pub fn format_generic<T: Into<f64> + Display>(value: T) -> String {
 }
 
 // Prints one input string per line surrounded by a box
-pub fn print_in_box(texts: Vec<String>){
+pub fn print_in_box(texts: Vec<String>) {
     let max_len = texts.iter().map(|s| s.len()).max().unwrap_or(0);
-    let horizontal_line = format!("#{:-<width$}#", "", width=max_len);
+    let horizontal_line = format!("#{:-<width$}#", "", width = max_len);
     println!("{}", horizontal_line);
 
-    for text in texts{
-        let line = format!("#{:-<width$}#", text, width=max_len);
+    for text in texts {
+        let line = format!("#{:-<width$}#", text, width = max_len);
         println!("{}", line);
     }
     println!("{}", horizontal_line);
 }
 
-pub fn parse_hexa_value(input: &Value) -> Result<u64, BlockRequestError>{
-
-    Ok(
-        u64::from_str_radix(
+pub fn parse_hexa_value(input: &Value) -> Result<u64, BlockRequestError> {
+    Ok(u64::from_str_radix(
         input
             .as_str()
-            .ok_or(BlockRequestError::ConversionError("Json block parameter to str"))?
+            .ok_or(BlockRequestError::ConversionError(
+                "Json block parameter to str",
+            ))?
             .trim_start_matches("0x"),
-        16).map_err(BlockRequestError::IntConversionError)?
+        16,
     )
+    .map_err(BlockRequestError::IntConversionError)?)
 }
